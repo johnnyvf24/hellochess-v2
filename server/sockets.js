78ca00f95a79de835e50be68261e0f134c0994c5
John V. F <johnnyvf24@gmail.com>
2017-02-04 20:09:03 -0700

    Handle move draws and checkmate conditions

--- server/sockets.js
+++ server/sockets.js
@@ -247,6 +247,100 @@ function userSittingAndGameOngoing(userObj, roomObj) {
     return false;
 }
 
+function endGame(io, timeType, wOldElo, lOldElo, winner, loser, roomIndex, roomName, draw) {
+    let elo = new Elo();
+
+    let wElo = elo.ifWins(wOldElo, lOldElo);
+    let lElo = elo.ifLoses(lOldElo, wOldElo);
+
+    if(draw) {
+        wElo = elo.ifTies(wOldElo, lOldElo);
+        lElo = elo.ifTies(lOldElo, wOldElo);
+    }
+
+    let updateObj = {};
+    updateObj[timeType] = wElo;
+
+    //save the winner's elo
+    User.findById({ _id: winner._id })
+    .then((user) => {
+        user.two_elos[timeType] = wElo;
+        user.save(function(err, updatedUser) {
+            let eloNotif = {
+                title: `${winner.username}'s elo is now ${wElo} +${wElo - wOldElo}`,
+                position: 'tr',
+                autoDismiss: 5,
+            };
+            io.to(roomName).emit('action', Notifications.success(eloNotif));
+            delete updatedUser.tokens;
+            io.to(updatedUser.socket_id).emit('action', {
+                type: 'user-update',
+                payload: updatedUser
+            });
+        });
+    }).catch((e) => {
+
+    });
+
+    setTimeout(() => {
+        //Save the loser's elo
+        User.findById({ _id: loser._id })
+        .then((user) => {
+            user.two_elos[timeType] = lElo;
+            user.save(function(err, updatedUser) {
+                let eloNotif = {
+                    title: `${loser.username}'s elo is now ${lElo} ${lElo - lOldElo}`,
+                    position: 'tr',
+                    autoDismiss: 5,
+                };
+                io.to(roomName).emit('action', Notifications.error(eloNotif));
+                delete updatedUser.tokens;
+                io.to(updatedUser.socket_id).emit('action', {
+                    type: 'user-update',
+                    payload: updatedUser
+                });
+            });
+        }).catch((e) => {
+        });
+    }, 250);
+
+
+    //TODO save game
+
+    //Stop the clocks
+    delete rooms[roomIndex][roomName].game;
+
+    //kick both players from board and restart game
+    setTimeout(() => {
+        delete rooms[roomIndex][roomName].white;
+        delete rooms[roomIndex][roomName].black;
+        let boardNotif = {
+            title: 'Board ready',
+            position: 'tr',
+            autoDismiss: 5,
+        };
+        io.to(roomName).emit('action', Notifications.warning(boardNotif));
+
+        io.to(roomName).emit('action', {
+            type: 'game-over',
+            payload: roomName
+        });
+
+        //Update the information about that room
+        io.emit('action', {
+            type: 'all-rooms',
+            payload: rooms
+        });
+
+        io.to(roomName).emit('action', {
+            type: 'resume',
+            payload: {
+                thread: roomName
+            }
+        });
+    }, 4000);
+}
+
 //Synchronize clocks every half second
 function initTimerSync(io, roomName, index) {
     let synchronizer = setInterval(() =>{
@@ -299,7 +393,7 @@ module.exports = function(io) {
     io.on('connection', (socket) => {
         socket.on('action', (action) => {
             let roomName, roomObj, userObj, roomIndex, color, index, turn;
-            let loser, winner;
+            let loser, winner, timeType, wOldElo, lOldElo;
             switch(action.type) {
                 //Client emiits message this after loading page
                 case 'server/connected-user':
@@ -387,98 +481,13 @@ module.exports = function(io) {
                         }
                     });
 
-                    let elo = new Elo();
-
                     //get elos and calculate new elos
-                    let timeType = getTimeTypeForTimeControl(rooms[roomIndex][roomName]);
+                    timeType = getTimeTypeForTimeControl(rooms[roomIndex][roomName]);
 
-                    let wOldElo = getEloForTimeControl(rooms[roomIndex][roomName], winner);
-                    let lOldElo = getEloForTimeControl(rooms[roomIndex][roomName], loser);
-                    let wElo = elo.ifWins(wOldElo, lOldElo);
-                    let lElo = elo.ifLoses(lOldElo, wOldElo);
+                    wOldElo = getEloForTimeControl(rooms[roomIndex][roomName], winner);
+                    lOldElo = getEloForTimeControl(rooms[roomIndex][roomName], loser);
 
-                    let updateObj = {};
-                    updateObj[timeType] = wElo;
-
-                    //save the winner's elo
-                    User.findById({ _id: winner._id })
-                    .then((user) => {
-                        user.two_elos[timeType] = wElo;
-                        user.save(function(err, updatedUser) {
-                            let eloNotif = {
-                                title: `${winner.username}'s elo is now ${wElo} +${wElo - wOldElo}`,
-                                position: 'tr',
-                                autoDismiss: 5,
-                            };
-                            io.to(roomName).emit('action', Notifications.success(eloNotif));
-                            delete updatedUser.tokens;
-                            io.to(updatedUser.socket_id).emit('action', {
-                                type: 'user-update',
-                                payload: updatedUser
-                            });
-                        });
-                    }).catch((e) => {
-
-                    });
-
-                    setTimeout(() => {
-                        //Save the loser's elo
-                        User.findById({ _id: loser._id })
-                        .then((user) => {
-                            user.two_elos[timeType] = lElo;
-                            user.save(function(err, updatedUser) {
-                                let eloNotif = {
-                                    title: `${loser.username}'s elo is now ${lElo} ${lElo - lOldElo}`,
-                                    position: 'tr',
-                                    autoDismiss: 5,
-                                };
-                                io.to(roomName).emit('action', Notifications.error(eloNotif));
-                                delete updatedUser.tokens;
-                                io.to(updatedUser.socket_id).emit('action', {
-                                    type: 'user-update',
-                                    payload: updatedUser
-                                });
-                            });
-                        }).catch((e) => {
-                        });
-                    }, 250);
-
-
-                    //TODO save game
-
-                    //Stop the clocks
-                    delete rooms[roomIndex][roomName].game;
-                    delete rooms[roomIndex][roomName].white;
-                    delete rooms[roomIndex][roomName].black;
-
-                    //kick both players from board and restart game
-                    setTimeout(() => {
-                        let boardNotif = {
-                            title: 'Board ready',
-                            position: 'tr',
-                            autoDismiss: 5,
-                        };
-                        io.to(roomName).emit('action', Notifications.warning(boardNotif));
-
-                        io.to(roomName).emit('action', {
-                            type: 'game-over',
-                            payload: roomName
-                        });
-
-                        //Update the information about that room
-                        io.emit('action', {
-                            type: 'all-rooms',
-                            payload: rooms
-                        });
-
-                        io.to(roomName).emit('action', {
-                            type: 'resume',
-                            payload: {
-                                thread: roomName
-                            }
-                        });
-                    }, 4000);
-
+                    endGame(io, timeType, wOldElo, lOldElo, winner, loser, roomIndex, roomName, false);
 
                     break;
 
@@ -493,27 +502,6 @@ module.exports = function(io) {
 
                     //make the move
                     move = rooms[index][roomName].game.move(move);
-                    if(rooms[index][roomName].game.game_over()) {
-
-                        //get the loser
-                        let nextTurn = rooms[index][roomName].game.turn();
-
-                        nextTurn = formatTurn(nextTurn);
-
-                        const winner = rooms[index][roomName][turn].username;
-                        const loser = rooms[index][roomName][nextTurn].username;
-
-                        //Notify all players that the game is ready to be played
-                        const notificationOpts = {
-                            title: 'Game Over',
-                            message: `${winner} is the winner`,
-                            position: 'tr',
-                            autoDismiss: 5,
-                        };
-
-                        io.to(roomName).emit('action', Notifications.warning(notificationOpts));
-
-                    }
                     if(move === null) {
                         //handle cheating scenario
                     } else {
@@ -534,6 +522,61 @@ module.exports = function(io) {
                             }
                         })
                     }
+
+                    //check to see if the game is over
+                    if(rooms[index][roomName].game.game_over()) {
+
+                        //get the loser
+                        let nextTurn = rooms[index][roomName].game.turn();
+
+                        nextTurn = formatTurn(nextTurn);
+
+                        winner = rooms[index][roomName][turn].username;
+                        loser = rooms[index][roomName][nextTurn].username;
+
+                        if(rooms[index][roomName].game.in_draw()) {
+                            //Notify all players that the game is ready to be played
+                            const notificationOpts = {
+                                title: 'Game Over',
+                                message: `The game ended in a draw!`,
+                                position: 'tr',
+                                autoDismiss: 5,
+                            };
+
+                            io.to(roomName).emit('action', Notifications.warning(notificationOpts));
+
+                            winner = rooms[index][roomName][turn];
+                            loser = rooms[index][roomName][nextTurn];
+
+                            timeType = getTimeTypeForTimeControl(rooms[index][roomName]);
+
+                            wOldElo = getEloForTimeControl(rooms[index][roomName], winner);
+                            lOldElo = getEloForTimeControl(rooms[index][roomName], loser);
+
+                            endGame(io, timeType, wOldElo, lOldElo, winner, loser, index, roomName, true);
+                        } else {
+                            //Notify all players that the game is ready to be played
+                            const notificationOpts = {
+                                title: 'Game Over',
+                                message: `${winner} is the winner`,
+                                position: 'tr',
+                                autoDismiss: 5,
+                            };
+
+                            io.to(roomName).emit('action', Notifications.warning(notificationOpts));
+
+                            winner = rooms[index][roomName][turn];
+                            loser = rooms[index][roomName][nextTurn];
+
+                            timeType = getTimeTypeForTimeControl(rooms[index][roomName]);
+
+                            wOldElo = getEloForTimeControl(rooms[index][roomName], winner);
+                            lOldElo = getEloForTimeControl(rooms[index][roomName], loser);
+
+                            endGame(io, timeType, wOldElo, lOldElo, winner, loser, index, roomName, false);
+                        }
+                    }
+
                     break;
 
                 //User is requesting to play as a certain color
